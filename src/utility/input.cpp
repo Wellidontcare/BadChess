429a38976ed5b1118975237f552bdbde98ed150a
Joris Nonnast <djjorisdj@gmail.com>
2019-09-21 19:58:22 +0200

    fixed a bug where wrong input would make the game crash

--- src/utility/input.cpp
+++ src/utility/input.cpp
@@ -6,9 +6,9 @@
 namespace chess {
 //converts the string input to coordinates
 std::vector<Coordinates> parse_input(const std::string &from, const std::string &to) {
-  int x1 = number_lookup.at(from[0]);
+  int x1 = from[0] > 'h' ? 10 : number_lookup.at(from[0]);
   int y1 = BOARD_WIDTH - (from[1] - '0');
-  int x2 = number_lookup.at(to[0]);
+  int x2 = to[0] > 'h' ? 10 : number_lookup.at(to[0]);
   int y2 = BOARD_WIDTH - (to[1] - '0');
   return {{x1, y1},
           {x2, y2}};
